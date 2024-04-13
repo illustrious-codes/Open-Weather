b4e4e688a8d85e19997e0d92182589b496f938fc
illustrious-codes <onatayodavid105@gmail.com>
2024-04-13 05:41:15 -0700

    processing

--- js/script.js
+++ js/script.js
@@ -14,33 +14,38 @@ search.addEventListener("click", () => {
   fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIkey}`
   )
-    .then((reponse) => response.json())
+    .then((response) => response.json())
     .then((json) => {
       const image = document.querySelector(".weather-box img");
       const temperature = document.querySelector("weather-box .temperature");
       const description = document.querySelector("weather-box .description");
-      const humidity = document.querySelector("weather-box .humidity span");
-      const wind = document.querySelector("weather-box .wind span");
+      const humidity = document.querySelector("weather-details .humidity span");
+      const wind = document.querySelector("weather-details .wind span");
 
       switch (json.weather[0].main) {
         case "Clear":
-          image.src = "./images/sun_weather-removebg-preview.png";
+          image.src = "./images/clear.png";
           break;
-        case "Clear":
-          image.src = "./images/sun_weather-removebg-preview.png";
+        case "Rain":
+          image.src = "./images/rain.png";
           break;
-        case "Clear":
-          image.src = "./images/sun_weather-removebg-preview.png";
+        case "Snow":
+          image.src = "./images/snow.png";
           break;
-        case "Clear":
-          image.src = "./images/sun_weather-removebg-preview.png";
+        case "Clouds":
+          image.src = "./images/cloud.png";
           break;
-        case "Clear":
-          image.src = "./images/sun_weather-removebg-preview.png";
+        case "Mist":
+          image.src = "./images/mist.png";
           break;
+        case "Haze":
+          image.src = "./images/mist.png";
+          break;
+        default:
+          image.src = "./images/cloud.png";
       }
 
-      temperature.innerHTML = `${parseInt(json.main.temp)} <span></span>`;
+      temperature.innerHTML = `${parseInt(json.main.temp)} <span>C</span>`;
       description.innerHTML = `${json.weather[0].description}`;
       humidity.innerHTML = `${json.main.humidity}%`;
       wind.innerHTML = `${parseInt(json.wind.speed)}km/h`;
